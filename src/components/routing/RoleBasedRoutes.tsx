0abd1f7beb18aebe02596d42c9d32ce3c63218f0
mavcon <maverickconsortium@gmail.com>
2024-12-08 20:10:19 -0500

    Timer enhancements: Improved up/down arrows styling with equal padding and simplified design. Added audio files in public directory for timer sounds.

--- src/components/routing/RoleBasedRoutes.tsx
+++ src/components/routing/RoleBasedRoutes.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Members from '../../pages/Members';
-import Settings from '../../pages/Settings';
-import Dojos from '../../pages/Dojos';
-import Stats from '../../pages/Stats';
-import Chat from '../../pages/Chat';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Members from "../../pages/Members";
+import Settings from "../../pages/Settings";
+import Dojos from "../../pages/Dojos";
+import Stats from "../../pages/Stats";
+import Chat from "../../pages/Chat";
+import Tools from "../../pages/Tools";
 
 interface RoleBasedRoutesProps {
   userRole: string;
   currentUserId: string;
 }
 
-const RoleBasedRoutes: React.FC<RoleBasedRoutesProps> = ({ userRole, currentUserId }) => {
+const RoleBasedRoutes: React.FC<RoleBasedRoutesProps> = ({
+  userRole,
+  currentUserId,
+}) => {
   return (
     <Routes>
       <Route path="/" element={<Members currentUserId={currentUserId} />} />
       <Route path="/stats" element={<Stats currentUserId={currentUserId} />} />
-      <Route path="/members/*" element={<Members currentUserId={currentUserId} />} />
+      <Route
+        path="/members/*"
+        element={<Members currentUserId={currentUserId} />}
+      />
       <Route path="/dojos" element={<Dojos currentUserId={currentUserId} />} />
-      <Route path="/settings" element={<Settings currentUserId={currentUserId} />} />
-      <Route path="/chat/:memberId" element={<Chat currentUserId={currentUserId} />} />
-      <Route path="/chat/group/:groupId" element={<Chat currentUserId={currentUserId} />} />
+      <Route path="/tools" element={<Tools />} />
+      <Route
+        path="/settings"
+        element={<Settings currentUserId={currentUserId} />}
+      />
+      <Route
+        path="/chat/:memberId"
+        element={<Chat currentUserId={currentUserId} />}
+      />
+      <Route
+        path="/chat/group/:groupId"
+        element={<Chat currentUserId={currentUserId} />}
+      />
     </Routes>
   );
 };
